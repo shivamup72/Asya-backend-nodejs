a07873739cdadd5c4300b55b7a5a6321d5ff344b
Akki921 <akshayclg36@gmail.com>
2022-03-10 10:20:17 +0530

    new chnages

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -6,15 +6,15 @@ const saltRounds = 10;
 // Create and Save a new Tutorial
 exports.login = async (req, res) => {
   // Validate request
-  let encryptedPassword ;
+  let encryptedPassword;
   if (!req.body.email && !req.body.password) {
     res.status(400).send({
       message: "Content can not be empty!",
-      status:false
+      status: false,
     });
     return;
   }
-  
+
   console.log("encryptedPassword", encryptedPassword);
   let dd = await Login.findOne({
     where: { email: req.body.email },
@@ -23,55 +23,43 @@ exports.login = async (req, res) => {
   console.log("dd", dd);
   if (dd) {
     encryptedPassword = await bcrypt.compare(
-        req.body.password,
-        dd.dataValues.password
-      );
-      if (encryptedPassword === true) {
-        res.status(200).send({
-            message: "Login Succeed!",
-            status:true
-          });
-      } else {
-        res.status(400).send({
-            message: "Please Enter Valid Password !",
-            status:false
-          });
-      }
-  } else {
+      req.body.password,
+      dd.dataValues.password
+    );
+    if (encryptedPassword === true) {
+      res.status(200).send({
+        message: "Login Succeed!",
+        status: true,
+        data: dd,
+      });
+    } else {
       res.status(400).send({
+        message: "Please Enter Valid Password !",
+        status: false,
+      });
+    }
+  } else {
+    res.status(400).send({
       message: "Please Enter Valid EmailId !",
-      status:false
+      status: false,
     });
   }
-  // // Create a Tutorial
-  // const  role= {
-  //   type: req.body.type,
-  // };
-  // // Save Tutorial in the database
-  // Role.create(role)
-  //   .then(data => {
-  //     res.send(data);
-  //   })
-  //   .catch(err => {
-  //     res.status(500).send({
-  //       message:
-  //         err.message || "Some error occurred while creating the role."
-  //     });
-  //   });
 };
 
-//   /////
-//   exports.findAll = (req, res) => {
-//     const title = req.query.title;
-//     var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-//     Tutorial.findAll({ where: condition })
-//       .then(data => {
-//         res.send(data);
-//       })
-//       .catch(err => {
-//         res.status(500).send({
-//           message:
-//             err.message || "Some error occurred while retrieving tutorials."
-//         });
-//       });
-//   };
+/////
+exports.loginProfile = (req, res) => {
+  console.log("req.params.id", req.params.id);
+  //var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  Login.findOne({
+    where: { id: req.params.id },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
--- app/routes/login.routes.js
+++ app/routes/login.routes.js
@@ -7,7 +7,7 @@ module.exports = app => {
     // // Retrieve all published Tutorials
     // router.get("/published", tutorials.findAllPublished);
     // // Retrieve a single Tutorial with id
-    // router.get("/:id", tutorials.findOne);
+    router.get("/:id", Login.loginProfile);
     // // Update a Tutorial with id
     // router.put("/:id", tutorials.update);
     // // Delete a Tutorial with id
