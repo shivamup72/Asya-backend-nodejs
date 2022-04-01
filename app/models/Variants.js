ea1b3be095a9a5658d68a72da194b1aeb2528518
Akki921 <akshayclg36@gmail.com>
2022-04-01 18:18:40 +0530

    update variants

--- app/models/Variants.js
+++ app/models/Variants.js
@@ -1,14 +1,14 @@
 module.exports = (sequelize, Sequelize) => {
-    const  Variants= sequelize.define("variants", {
-        menu_id: {
-            type: Sequelize.INTEGER,
-          },
-          name: {
-            type: Sequelize.STRING,
-          },
-          options :{
-            type: Sequelize.TEXT,
-          },
-    });
-    return Variants;
-  };
+  const Variants = sequelize.define("variants", {
+    menu_id: {
+      type: Sequelize.INTEGER,
+    },
+    variant: {
+      type: Sequelize.TEXT,
+    },
+    price: {
+      type: Sequelize.STRING,
+    },
+  });
+  return Variants;
+};
