a4729fe57159b40a38a9d76997a10dd720b5aa47
Akki921 <akshayclg36@gmail.com>
2022-04-19 16:39:21 +0530

    oreder api update

--- app/models/Orders.js
+++ app/models/Orders.js
@@ -1,8 +1,53 @@
 module.exports = (sequelize, Sequelize) => {
-    const  Orders= sequelize.define("orders", {
-        type: {
-        type: Sequelize.STRING
-      },
-    });
-    return Role;
-  };
+  const Orders = sequelize.define("orders", {
+    code: {
+      type: Sequelize.STRING,
+    },
+    customer_id: {
+      type: Sequelize.INTEGER,
+    },
+
+    customer_address_id: {
+      type: Sequelize.INTEGER,
+    },
+    driver_id: {
+      type: Sequelize.INTEGER,
+    },
+    order_placed_at: {
+      type: Sequelize.INTEGER,
+    },
+    order_approved_at: {
+      type: Sequelize.INTEGER,
+    },
+    order_preparing_at: {
+    },
+    order_prepared_at: {
+      type: Sequelize.INTEGER,
+    },
+    order_delivered_at: {
+      type: Sequelize.INTEGER,
+    },
+    order_canceled_at: {
+      type: Sequelize.INTEGER,
+    },
+    order_status: {
+      type: Sequelize.STRING,
+    },
+    note: {
+      type: Sequelize.TEXT,
+    },
+    total_menu_price: {
+      type: Sequelize.STRING,
+    },
+    total_delivery_charge: {
+      type: Sequelize.STRING,
+    },
+    total_vat_amount: {
+      type: Sequelize.STRING,
+    },
+    grand_total: {
+      type: Sequelize.STRING,
+    },
+  });
+  return Orders;
+};
