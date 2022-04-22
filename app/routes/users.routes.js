4d48edc530efb859ef878784381ee08311ba0f8a
Akki921 <akshayclg36@gmail.com>
2022-04-22 17:28:19 +0530

    update ss

--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -6,7 +6,7 @@ module.exports = app => {
     // // Retrieve all Tutorials
     router.get("/getuser", users.findAll);
     // // Retrieve all published Tutorials
-    // router.get("/published", tutorials.findAllPublished);
+   router.post("/updateprofile", users.updatepassword);
     // // Retrieve a single Tutorial with id
     // router.get("/:id", tutorials.findOne);
     // // Update a Tutorial with id
