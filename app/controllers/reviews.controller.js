438fe816414370086daf499e883b5dc6ad69916d
Akki921 <akshayclg36@gmail.com>
2022-05-04 11:44:37 +0530

    update

--- app/controllers/reviews.controller.js
+++ app/controllers/reviews.controller.js
@@ -4,12 +4,11 @@ const Reviews = db.Reviews;
 const Op = db.Sequelize.Op;
 // Create and Save a new Tutorial
 exports.create = async (req, res) => {
-  const { add} =
-    req.body;
+  const { add } = req.body;
   console.log("first,", req.body);
 
   if (
-    !add.order_code &&
+    !add.ordercode &&
     !add.menu_id &&
     !add.customer_id &&
     !add.rating &&
@@ -21,66 +20,64 @@ exports.create = async (req, res) => {
     });
     return;
   }
-//   let check = await Cart.findOne({
-//     where: { customer_id: customer_id, menu_id: menu_id },
-//   });
-//   console.log("first", check);
-//   if (check !== null) {
-//     Cart.update(
-//       {
-//         order_code : order_code,
-//     menu_id :menu_id ,
-//     customer_id : customer_id,
-//     rating :rating ,
-//     review : review,
-//     restaurant_id: restaurant_id,
-//     timestamp :timestamp
-//       },
-//       { where: { customer_id: customer_id, menu_id: menu_id } }
-//     )
-//       .then((result) =>
-//         res.status(200).send({
-//           status: true,
-//           message: "cart update  Successfully",
-//         })
-//       )
-//       .catch((err) =>
-//         res.status(500).send({
-//           status: false,
-//           message: err + "cart update   UnSuccessfully",
-//         })
-//       );
-//   } else {
-    // Create a Tutorial
-    const reviews = {
-        order_code :add. order_code,
-        menu_id :add.menu_id ,
-        customer_id :add. customer_id,
-        rating :add.rating ,
-        review :add. review,
-        restaurant_id:add. restaurant_id
-    };
-    // Save Tutorial in the database
-    Reviews.create(reviews)
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while creating the review.",
-        });
+    let check = await Reviews.findOne({
+      where: { customer_id: add.customer_id,order_code: add.ordercode },
+    });
+    console.log("first", check);
+    if (check !== null) {
+      Reviews.update(
+        {
+          order_code: add.ordercode,
+          menu_id: add.menu_id,
+          customer_id: add.customer_id,
+          rating: add.rating,
+          review: add.review,
+          restaurant_id: add.restaurant_id,
+          timestamp:parseInt(Date.now()/1000)
+        },
+        { where: { customer_id: add.customer_id,order_code: add.ordercode } }
+      )
+        .then((result) =>
+          res.status(200).send({
+            status: true,
+            message: "cart update  Successfully",
+          })
+        )
+        .catch((err) =>
+          res.status(500).send({
+            status: false,
+            message: err + "cart update   UnSuccessfully",
+          })
+        );
+    } else {
+  // Create a Tutorial
+  const reviews = {
+    order_code: add.ordercode,
+    menu_id: add.menu_id,
+    customer_id: add.customer_id,
+    rating: add.rating,
+    review: add.review,
+    restaurant_id: add.restaurant_id,
+  };
+  // Save Tutorial in the database
+  Reviews.create(reviews)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the review.",
       });
-  
+    });
+  }
 };
 
 // /////
 exports.findAll = (req, res) => {
   // const title = req.query.title;
   // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-  Reviews.findAll({
-   
-  })
+  Reviews.findAll({})
     .then((data) => {
       res.send(data);
     })
@@ -92,9 +89,8 @@ exports.findAll = (req, res) => {
 };
 
 exports.findOne = (req, res) => {
-    Reviews.findAll({
-    where: { customer_id: req.params.id }
-    
+  Reviews.findAll({
+    where: { customer_id: req.params.id },
   })
     .then((data) => {
       if (!data || data === null) {
